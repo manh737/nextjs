8ec711c5f6fbc994423f3484521c95f26f63c1c3
Manh Nguyen <nguyenducmanh.sla@gmail.com>
2022-12-21 22:37:22 +0700

    lesson 2

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,7 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
-import Link from 'next/link'
+import Head from "next/head";
+import Image from "next/image";
+import styles from "../styles/Home.module.sass";
+import Link from "next/link";
 
 export default function Home() {
   return (
@@ -14,11 +14,11 @@ export default function Home() {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-         Read <Link href="/posts/first-post">first post</Link>
+          Read <Link href="/posts/first-post">first post</Link>
         </h1>
 
         <p className={styles.description}>
-          Get started by editing{' '}
+          Get started by editing{" "}
           <code className={styles.code}>pages/index.tsx</code>
         </p>
 
@@ -61,12 +61,12 @@ export default function Home() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Powered by{' '}
+          Powered by{" "}
           <span className={styles.logo}>
             <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
           </span>
         </a>
       </footer>
     </div>
-  )
+  );
 }
